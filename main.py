3e3c3ead885de37743853aa8cf6acc5837ea9579
CarlosAC05 <134300063+CarlosAC05@users.noreply.github.com>
2023-05-23 16:28:27 -0400

    Add files via upload
    Just basic interfacing

---
+++ main.py
@@ -0,0 +1,24 @@
+import openai
+
+prompt = input("Enter A Number from 1-100 based on your current mood ")
+def generate_text(prompt):
+    openai.api_key = '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'
+    models = openai.Model.list()
+    print(models.data[2].id)
+    Chat_Completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": ("I am " + str(prompt) + "on a scale from 1-100 where 100 is very happy and 1 is extremly depressed.")},{"role": "system", "content":("pretend you are a drill seargent")}],
+        #messages= [],
+        temperature =.1,
+        max_tokens=80
+    )
+    print(Chat_Completion.choices[0].message.content)
+
+
+    # Extract the generated text from the API response
+
+
+# Prompt the user for input
+
+
+generate_text(prompt)
